9078f4a2a9e49f66b04bdf04cd1b23823e077e32
kofshikz <kofshik@hotmail.com>
2022-12-10 23:56:24 +0300

    A little bit of refactoring

--- 2022/Day 1/main.go
+++ 2022/Day 1/main.go
@@ -9,21 +9,21 @@ import (
 	"strings"
 )
 
-var element int
-var maxTotalCalories int = 0
+var allCalories []int
+var topThree = 0
 
 func main() {
-	allCalories := []int{}
 	content, err := os.ReadFile("calories.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	elves := strings.Split(string(content), "\n\n")
-	// fmt.Println(v[1])
-	for i := 0; i < len(elves); i++ {
-		elfCalories := strings.Split(elves[i], "\n")
-		var totalCalories int = 0
-		for _, element := range elfCalories {
+	// This will make the code Windows compatible
+	allElves := strings.ReplaceAll(string(content), "\r\n", "\n")
+	groupedCalories := strings.Split(allElves, "\n\n")
+	for i := 0; i < len(groupedCalories); i++ {
+		calorieGroup := strings.Split(groupedCalories[i], "\n")
+		var totalCalories = 0
+		for _, element := range calorieGroup {
 			element, err := strconv.Atoi(element)
 			if err != nil {
 				log.Fatal(err)
@@ -32,18 +32,14 @@ func main() {
 		}
 		allCalories = append(allCalories, totalCalories)
 	}
-
 	sort.Slice(allCalories, func(i, j int) bool {
 		return allCalories[i] > allCalories[j]
 	})
-	var topThreeSum = 0
+	// Part 1
+	fmt.Printf("The Elf carrying the most Calories carries %d of them\n", allCalories[0])
+	// Part 2
 	for _, element := range allCalories[:3] {
-		topThreeSum += element
+		topThree += element
 	}
-	var topCalories = 0
-	for _, element := range allCalories[:1] {
-		topCalories += element
-	}
-	fmt.Println("Top 3 sum: ", topThreeSum)
-	fmt.Println(topCalories)
+	fmt.Printf("The top three Elves are carrying %d calories combined\n", topThree)
 }
